052b8ab0116a6b338202d4c85be149c229a0d1df
theunrealkosyak@gmail.com <Gfkmvf4578>
2021-08-10 13:11:29 +0300

    Range task. Intersection complete

--- Range/src/oop/range/Range.java
+++ Range/src/oop/range/Range.java
@@ -4,7 +4,13 @@ public class Range {
     private double from;
     private double to;
 
-    public Range(){}
+    public Range() {
+    }
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
 
     public double getFrom() {
         return from;
@@ -22,6 +28,72 @@ public class Range {
         this.to = to;
     }
 
+    public double size() {
+        return to - from;
+    }
 
+    public boolean isInside(double value) {
+        return (from <= value && value <= to);
+    }
 
+    public Range intersection(Range range) {
+        if (range.getFrom() == to || range.getTo() == from){
+            return null;
+        }
+
+        Range newRange = new Range();
+
+        //range inside range from start
+        if (from < range.getFrom() && range.getFrom() < to) {
+            newRange.setFrom(range.getFrom());
+
+            if (from < range.getTo() && range.getTo() < to){
+                newRange.setTo(range.getTo());
+            } else {
+                newRange.setTo(to);
+            }
+
+            return newRange;
+        }
+
+        //range inside range only for end
+        if (from < range.getTo() && range.getTo() < to) {
+            newRange.setFrom(from);
+            newRange.setTo(range.getTo());
+
+            return newRange;
+        }
+
+        if (range.getFrom() < from && from < range.getTo()) {
+            newRange.setFrom(from);
+
+            if (range.getFrom() < to && to < range.getTo()){
+                newRange.setTo(to);
+            } else {
+                newRange.setTo(range.getTo());
+            }
+
+            return newRange;
+        }
+
+        if (range.getFrom() < to && to < range.getTo()) {
+            newRange.setFrom(range.from);
+            newRange.setTo(to);
+
+            return newRange;
+        }
+
+        return null;
+    }
+
+    public Range[] merge(Range range) {
+        Range[] newRange = new Range[2];
+
+        if (isInside(range.getFrom()) && isInside(range.getTo())){
+            Range firstRange = new Range(from, to);
+            newRange[0] = firstRange;
+        }
+
+        return newRange;
+    }
 }
--- Range/src/range/application/Main.java
+++ Range/src/range/application/Main.java
@@ -1,7 +1,50 @@
 package range.application;
 
+import oop.range.Range;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Create range");
+
+        Range range = new Range(-4.2, 2.5);
+
+        System.out.println("Range created: from " + range.getFrom() + " to " + range.getTo());
+        System.out.println("Range has size: " + range.size());
+
+        double insideNumber =-4.25;
+
+        System.out.println("Number " + insideNumber + " inside in range?");
+
+        boolean isInside = range.isInside(insideNumber);
+
+        if (isInside) {
+            System.out.println(insideNumber + " include in range");
+        } else {
+            System.out.println(insideNumber + " exclude from range");
+        }
+
+        System.out.println("-----Base task was tested-----");
+        System.out.println("Intersection");
+
+        Range secondRange = new Range (-3.6, -3.0);
+        Range newRange = range.intersection(secondRange);
+
+        if (newRange != null){
+            System.out.println(newRange.getFrom() + "|" + newRange.getTo());
+        } else {
+            System.out.println("Intersection not exist");
+        }
+
+        System.out.println("Merge of range");
+
+        Range[] rangeArray = new Range[2];
+                rangeArray = range.merge(secondRange);
+
+        if (rangeArray[1] == null){
+            System.out.println("Merged range: [" + rangeArray[0].getFrom() + ";" + rangeArray[0].getTo() + "]");
+        } else {
+            System.out.println("Merge not exist. Result: " + rangeArray[0] + ";" + rangeArray[1]);
+        }
 
     }
 }
