68a8647a5e727ba214a0ce964c361239ffa0126c
Konstantin Kolchurin <support@mellman.ru>
2021-09-21 23:39:02 +0700

    Rework RangeApplication.java

--- Range/src/kks/oop/range/application/RangeApplication.java
+++ Range/src/kks/oop/range/application/RangeApplication.java
@@ -15,17 +15,31 @@ public class RangeApplication {
 
         printDivider();
 
-        calculateIntersection();
+        Range intersectionRange1 = new Range(-7.0, 19.0);
+        Range intersectionRange2 = new Range(15.0, 35.0);
+
+        calculateIntersection(intersectionRange1, intersectionRange2);
 
         printDivider();
 
-        calculateUnion();
+        Range unionRange1 = new Range(1.0, 3.0);
+        Range unionRange2 = new Range(5.0, 9.0);
+
+        calculateUnion(unionRange1, unionRange2);
 
         printDivider();
 
-        calculateDifference();
+        Range differenceRange1 = new Range(5.0, 7.0);
+        Range differenceRange2 = new Range(3.0, 7.0);
+
+        calculateDifference(differenceRange1, differenceRange2);
 
         printDivider();
+/*
+        Range urange1 = new Range(1.0, 3.0);
+        Range urange2 = new Range(5.0, 9.0);
+
+        test(urange1, urange2);*/
     }
 
     private static void calculateIsInside(Range range, int insideNumber) {
@@ -36,44 +50,42 @@ public class RangeApplication {
         System.out.println(insideNumber + " " + resultIsInsideToString(isInside));
     }
 
-    private static void calculateIntersection() {
+    private static void calculateIntersection(Range range1, Range range2) {
         System.out.println("Intersection:");
+        System.out.println(range1);
+        System.out.println(range2);
 
-        Range intersectionRange1 = new Range(-7.0, 19.0);
-        Range intersectionRange2 = new Range(15.0, 35.0);
+        Range intersectionRange = range1.getIntersection(range2);
 
-        System.out.println(intersectionRange1);
-        System.out.println(intersectionRange2);
-
-        Range intersectionRange = intersectionRange1.getIntersection(intersectionRange2);
-
-        printResultIntersection(intersectionRange);
+        if (intersectionRange != null) {
+            System.out.println("Intersection: " + intersectionRange);
+        } else {
+            System.out.println("Intersection not exist");
+        }
     }
 
-    private static void calculateUnion() {
+    private static void calculateUnion(Range range1, Range range2) {
         System.out.println("Union:");
+        System.out.println(range1);
+        System.out.println(range2);
 
-        Range unionRange1 = new Range(1.0, 3.0);
-        Range unionRange2 = new Range(5.0, 9.0);
+        Range[] unionRanges = range1.getUnion(range2);
 
-        System.out.println(unionRange1);
-        System.out.println(unionRange2);
+        if (unionRanges.length == 1) {
+            System.out.println("Result: " + unionRanges[0]);
+        } else {
+            System.out.println("Result: ");
 
-        Range[] unionRanges = unionRange1.getUnion(unionRange2);
-
-        printResultUnionRanges(unionRanges);
+            printRanges(unionRanges);
+        }
     }
 
-    private static void calculateDifference() {
+    private static void calculateDifference(Range range1, Range range2) {
         System.out.println("Difference:");
+        System.out.println(range1);
+        System.out.println(range2);
 
-        Range differenceRange1 = new Range(5.0, 7.0);
-        Range differenceRange2 = new Range(1.0, 3.0);
-
-        System.out.println(differenceRange1);
-        System.out.println(differenceRange2);
-
-        Range[] differenceRanges = differenceRange1.getDifference(differenceRange2);
+        Range[] differenceRanges = range1.getDifference(range2);
 
         System.out.println("Result:");
 
@@ -102,25 +114,8 @@ public class RangeApplication {
         System.out.println("].");
     }
 
-    private static void printResultIntersection(Range range) {
-        if (range != null) {
-            System.out.println("Intersection: " + range);
-        } else {
-            System.out.println("Intersection not exist");
-        }
-    }
-
-    private static void printResultUnionRanges(Range[] array) {
-        if (array.length == 1) {
-            System.out.println("Union: " + array[0]);
-        } else {
-            System.out.println("Union not exist. Result:");
-
-            printRanges(array);
-        }
-    }
-
     private static void printDivider() {
         System.out.println("###################");
     }
+
 }
